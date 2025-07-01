d13e861578a72028149592b074475bd07a51b192
Robben <Robben.Juan@gmail.com>
2025-07-01 10:48:10 +0900

    made hamburger menu for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,11 +7,17 @@ import { useRouter } from "next/navigation";
 
 export default function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
   const router = useRouter();
 
   useEffect(() => {
     setIsLoggedIn(!!localStorage.getItem("token"));
   }, []);
+
+  const hamburgerMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -32,8 +38,67 @@ export default function Navbar() {
         <span className="text-red-500">Recipe</span>Aid
       </Link>
 
-      <div className="flex items-center gap-4">
-        <Link href="/mealbuilder" className="text-gray-700 hover:text-red-500">
+      {/* Mobile Menu Button */}
+      <div className="flex sm:hidden">
+        <button
+          onClick={hamburgerMenu}
+          aria-expanded={isOpen}
+          className=" group h-9 w-13   flex flex-col justify-center items-center gap-1.5"
+        >
+          <span
+            className={`${
+              isOpen
+                ? "h-1 w-6 rounded-full bg-black transition rotate-45 translate-y-2.5"
+                : "h-1 w-6 rounded-full bg-black transition"
+            }`}
+          ></span>
+          <span
+            className={`${
+              isOpen
+                ? "scale-x-0 transition h-1 w-6 rounded-full bg-black"
+                : "h-1 w-6 rounded-full bg-black transition"
+            }`}
+          ></span>
+          <span
+            className={`${
+              isOpen
+                ? "h-1 w-6 rounded-full bg-black transition -rotate-45 -translate-y-2.5"
+                : "h-1 w-6 rounded-full bg-black transition"
+            }`}
+          ></span>
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="absolute top-[70px] left-0 w-full flex flex-col items-center gap-4 bg-white py-4 shadow-md sm:hidden z-50 transition-all duration-300">
+          <Link
+            href="/mealbuilder"
+            className=" text-gray-700 hover:text-red-500"
+          >
+            Meal Builder
+          </Link>
+
+          {isLoggedIn ? (
+            <button
+              onClick={handleLogout}
+              className="text-white bg-red-500 hover:bg-red-600 px-3 py-1 rounded"
+            >
+              Logout
+            </button>
+          ) : (
+            <Link
+              href="/login"
+              className="border border-gray-300 text-gray-800 px-3 py-1 rounded hover:bg-gray-100"
+            >
+              Login
+            </Link>
+          )}
+        </div>
+      )}
+
+      <div className="hidden sm:flex items-center gap-4">
+        <Link href="/mealbuilder" className=" text-gray-700 hover:text-red-500">
           Meal Builder
         </Link>
 
